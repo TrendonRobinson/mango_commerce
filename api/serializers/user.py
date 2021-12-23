4b9f7b6eb7de13d3c5cfbf6a07fc3743a0a210b5
The_Pr0fessor <Trendonrobinson@gmail.com>
2021-12-23 01:37:34 -0500

    userfix

--- api/serializers/user.py
+++ api/serializers/user.py
@@ -7,8 +7,9 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ('id', 'email', 'password', 'username', 'first', 'last',
-                  'date_joined', 'last_login', 'is_admin', 'is_staff')
+        fields = ("id", "email", "password")
+        # fields = ('id', 'email', 'password', 'username', 'first', 'last',
+        #           'date_joined', 'last_login', 'is_admin', 'is_staff')
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
         # This create method will be used for model creation
