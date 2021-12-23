db6f182158d382b0363bf33530ec37773aa65b84
The_Pr0fessor <Trendonrobinson@gmail.com>
2021-12-23 01:42:39 -0500

    user check

--- api/serializers/user.py
+++ api/serializers/user.py
@@ -7,7 +7,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ("id", "email", "password")
+        fields = ("id", "email", "password", "first_name", "last_name")
         # fields = ('id', 'email', 'password', 'username', 'first', 'last',
         #           'date_joined', 'last_login', 'is_admin', 'is_staff')
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
